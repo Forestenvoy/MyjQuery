3df711884b6523e3c271714c41ffd1d79bea899b
Forestenvoy <bbtsnpo@gmail.com>
2024-03-10 23:45:16 +0800

    燈箱 文字追加完成

--- jQuery_lesson-master/practice/lightbox/lightbox.js
+++ jQuery_lesson-master/practice/lightbox/lightbox.js
@@ -5,9 +5,14 @@ $(".lightbox").on("click", function (e) {
     e.preventDefault();
     // 取得點擊圖片的 src
     var imageUrl = $(this).attr("href");
-    console.log(imageUrl);
+    // console.log(imageUrl);
     // 設置燈箱圖片
     $(".lightbox-img").attr("src", imageUrl);
+    // 取得點擊圖片的 alt
+    var imageText = $(this).find("img").attr("alt");
+    console.log(imageText);
+    // 設置燈箱文字
+    $(".text").text(imageText);
     $(".lightbox-overlay").fadeIn(500);
 });
 $(".close").on("click", function () {
